4f47e46242cbd8ae7691d557e943503959eb9f32
dhyey2502 <dhyey.25022003@gmail.com>
2023-08-03 17:51:17 +0530

    update

--- index.php
+++ index.php
@@ -21,52 +21,54 @@
 
     <div style="padding:25px 0">
 
-    <?php
-    $sql = "SELECT * FROM tbl_slider";
-    $result = $con->query($sql);
-    $data = array();
-    if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-    $data[] = $row;
-    }
-    }
-    ?>
+        <?php
+        $sql = "SELECT * FROM tbl_slider";
+        $result = $con->query($sql);
+        $data = array();
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        }
+        ?>
         <swiper-container class="mySwiper" navigation="true">
-        <?php foreach ($data as $item) { ?>
-        <?php if ($item['file_type']== 'image' ) {?>
-            <swiper-slide><img src="images/<?php echo $item['file_name'];?>" alt="slider_content"></swiper-slide>      
-        <?php  } else { ?>
-            <swiper-slide><video src="images/<?php echo $item['file_name'];?>?>" controls></video></swiper-slide>
-        <?php }
-        } ?>
+            <?php foreach ($data as $item) { ?>
+                <?php if ($item['file_type'] == 'image') { ?>
+                    <swiper-slide><img src="images/<?php echo $item['file_name']; ?>" alt="slider_content"></swiper-slide>
+                <?php } else { ?>
+                    <swiper-slide><video src="images/<?php echo $item['file_name']; ?>?>" controls></video></swiper-slide>
+                <?php }
+            } ?>
         </swiper-container>
     </div>
 
     <div style="padding:25px 0">
         <section class="about-us">
             <div class="about">
-            <?php
-    $sql = "SELECT * FROM tbl_about_us";
-    $result = $con->query($sql);
-    $data = array();
-    if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-    $data[] = $row;
-    }
-    }
-    ?>
 
+                <?php
+                $sql = "SELECT * FROM tbl_about_us";
+                $result = $con->query($sql);
+                $data = array();
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $data[] = $row;
+                    }
+                }
+                ?>
 
-<?php foreach ($data as $item) { ?>
-                <img src="images/<?php echo $item['image'];?>" class="pic">
-                <div class="text">
-                    <h2>About Us</h2>
-                    <p><?php echo $item['description'];?></p>
-                    <div class="data">
-                        <a href="#" class="about-btn">Know More</a>
+                <?php foreach ($data as $item) { ?>
+                    <img src="images/<?php echo $item['image']; ?>" class="pic">
+                    <div class="text">
+                        <h2>About Us</h2>
+                        <p>
+                            <?php echo $item['description']; ?>
+                        </p>
+                        <div class="data">
+                            <a href="#" class="about-btn">Know More</a>
+                        </div>
                     </div>
-                </div>
-                <?php }?>
+                <?php } ?>
             </div>
         </section>
     </div>

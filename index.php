7d767aef7fe0a137a5f08c470fe903b3a485a24c
khushijadav15 <khushijadav15@gmail.com>
2023-08-03 16:17:31 +0530

    update
    update

--- index.php
+++ index.php
@@ -20,11 +20,33 @@
     <div style="padding:25px 0">
         <swiper-container class="mySwiper" navigation="true">
             <swiper-slide><video src=""></video></swiper-slide>
-            <swiper-slide><img src="" alt="img1"></swiper-slide>
-            <swiper-slide><img src="" alt="img2"></swiper-slide>
-            <swiper-slide><img src="" alt="img3"></swiper-slide>
+            <swiper-slide><img src="images/pic-1.jpg" alt="img1"></swiper-slide>
+            <swiper-slide><img src="images/pic-2.jpg" alt="img2"></swiper-slide>
+            <swiper-slide><img src="images/pic-3.jpg" alt="img3"></swiper-slide>
         </swiper-container>
     </div>
+
+    <div style="padding:25px 0">
+        <section class="about-us">
+            <div class="about">
+                <img src="images/pic-4.jpg" class="pic">
+                <div class="text">
+                    <h2>About Us</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque
+                        ullam
+                        enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo
+                        cum
+                        vero atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem repudiandae
+                        cumque
+                        voluptate consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
+                    <div class="data">
+                        <a href="#" class="about-btn">Know More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"></script>

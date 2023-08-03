d56f273420cc8810106e85d9c28314d34edab85d
khushijadav15 <khushijadav15@gmail.com>
2023-08-03 15:41:51 +0530

    update
    update

--- index.php
+++ index.php
@@ -3,20 +3,21 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-    <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="#news">About Us</a>
-        <a href="#contact">Property</a>
-        <a href="#about">Contact Us</a>
+    <div class="nav-bar">
+        <a href="#home" class="active">Company</a>
+        <a href="#home">Home</a>
+        <a href="#property">Property</a>
+        <a href="#about">About Us</a>
+        <a href="#contact">Contact Us</a>
     </div>
 
     <div style="padding-left:16px">
-        <h2>Top Navigation Example</h2>
-        <p>Some content..</p>
+
     </div>
 
 </body>

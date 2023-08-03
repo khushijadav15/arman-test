0f995ac4198b01b6d4910e9b88214f575cd8911b
RIjvan-Juneja <junejarijvan8128@gmail.com>
2023-08-03 17:53:20 +0530

    update

---
+++ admin/import/sidebar.php
@@ -0,0 +1,24 @@
+<div class="quixnav">
+            <div class="quixnav-scroll">
+                <ul class="metismenu" id="menu">
+                    <li class="nav-label first">Main Menu</li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                                class="icon icon-single-04"></i><span class="nav-text">Slider</span></a>
+                        
+                    </li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                                class="icon icon-app-store"></i><span class="nav-text">About Us</span></a>
+                    </li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                                class="icon icon-app-store"></i><span class="nav-text">Proporties Card</span></a>
+                    </li>
+
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                                class="icon icon-app-store"></i><span class="nav-text">Form Data</span></a>
+                    </li>
+
+                </ul>
+            </div>
+
+
+        </div>

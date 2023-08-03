09ba76522d2f67b86e607a1a3db09d6304af0ada
RIjvan-Juneja <junejarijvan8128@gmail.com>
2023-08-03 16:48:39 +0530

    update

---
+++ admin/login.php
@@ -0,0 +1,55 @@
+<?php include '../Database/connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <?php include './import/head.php'; ?>
+</head>
+
+<body class="h-100">
+    <div class="authincation h-100">
+        <div class="container-fluid h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-4">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+                                    <h4 class="text-center mb-4">Welcome To Admin</h4>
+                                    <form action="index.html">
+                                        <div class="form-group">
+                                            <label><strong>Username</strong></label>
+                                            <input type="number" class="form-control" placeholder="9999999999">
+                                        </div>
+                                        <div class="form-group">
+                                            <label><strong>Password</strong></label>
+                                            <input type="password" class="form-control" placeholder="******">
+                                        </div>
+                                        
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary btn-block">Login</button>
+                                        </div>
+                                    </form>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="./vendor/global/global.min.js"></script>
+    <script src="./js/quixnav-init.js"></script>
+    <script src="./js/custom.min.js"></script>
+
+</body>
+
+</html>
